1313d3dc3864830c6e9b0eace12dbf9f163a825e
Krymeq <krymeq353@gmail.com>
2019-11-23 13:45:50 +0100

    Added simple contour detection

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from skimage import io
+from skimage import io, measure, img_as_float
 import matplotlib.pyplot as plt
+import cv2
 import numpy as np
-import skimage.morphology as mp
+from scipy import ndimage
 
 
 def thresh(number, threshpoint):
@@ -13,7 +14,7 @@ def thresh(number, threshpoint):
 
 def gamma(img):
     g = np.mean(img)
-    return (img**g)
+    return img**g
 
 
 def contrast(img):
@@ -28,15 +29,40 @@ def contrast(img):
 
 
 def prepare_photo(photo):
-    photo = gamma(photo)
-    # photo = gamma(photo)
-    #
-    # photo = contrast(photo)
+    work = cv2.cvtColor(photo, cv2.COLOR_BGR2GRAY)
+    _, work = cv2.threshold(work, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+
+    contour_array, _ = cv2.findContours(work, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+    contours = []
+
+    #Weź kontury dłuższe niż ileś tam pikseli
+    for xD in contour_array:
+        if cv2.contourArea(xD) > 60:
+            contours.append(xD)
+
+    cv2.drawContours(photo, contours, -1, (0, 255, 0), 3)
 
     return photo
 
 
-image = io.imread("./data/kostka.jpg", as_gray=True)
-io.imshow(prepare_photo(image))
+#  ][\\    //][       //\\       ][   ][\\    ][
+#  ][ \\  // ][      //  \\      ][   ][ \\   ][
+#  ][  \\//  ][     //====\\     ][   ][  \\  ][
+#  ][        ][    //      \\    ][   ][   \\ ][
+#  ][        ][   //        \\   ][   ][    \\][
 
-plt.show()
+if __name__ == '__main__':
+    filenames = []
+
+    for i in range(1, 12):
+        filenames.append(str(i))
+
+    filenames.append("kostka")
+
+    images = [cv2.imread('./data/' + i + '.jpg') for i in filenames]
+
+    for i in range(0, len(filenames)):
+        plt.clf()
+        io.imshow(prepare_photo(images[i]))
+        plt.savefig('./Preprocessed/' + filenames[i] + '.png')
